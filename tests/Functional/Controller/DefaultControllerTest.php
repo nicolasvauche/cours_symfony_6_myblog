35908673a6e87502ea4ec95948b2592d9a9a3593
Nicolas vauché <hello@nicolasvauche.net>
2023-12-19 13:11:20 +0100

    Tests fonctionnels pour la page d'accueil

---
+++ tests/Functional/Controller/DefaultControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Functional\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testHomepageAnswersCorrectly(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur mon blog !');
+    }
+
+    public function testError404(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/truc');
+
+        $this->assertResponseStatusCodeSame(408);
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+}
